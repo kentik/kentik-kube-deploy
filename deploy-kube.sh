7ce7bbd4c5f0c0694379ea0426b28dae85580ef4
Scott Dorr <sdorr@kentik.com>
2023-11-22 01:55:43 -0700

    kubemeta versioning, other tweaks

--- deploy-kube.sh
+++ deploy-kube.sh
@@ -12,16 +12,16 @@
 #
 # You must set these four as appropriate for your account and device
 #
-PLAN=5354
-EMAIL=tma+kentikdemonew@kentik.com
-TOKEN=c381ac67174620173f41d0c0dabdb07c
-CLUSTER=gke-cluster-kentik-standard
-CLOUDPROVIDER=gcp
+PLAN=0123456789
+EMAIL=customer_email@example.com
+TOKEN=123abc_your_customer_kentik_token_cba321
+CLUSTER=your_cluster_name
+CLOUDPROVIDER=aws_or_gcp_or_azure_or_prem
 #
 # Don't change the below values unless you know for sure it's what you want.
 #
 CAPTURE='en*|veth.*|eth*'
-KUBEMETA_VERSION=sha-c9723a0
+KUBEMETA_VERSION=sha-68b9457
 # ######################
 #
 #   END CONFIGURATION
@@ -86,25 +86,14 @@ if [[ ! "${confirmation}" =~ ^[yY]$ ]]; then
 fi
 
 sed \
-    -e 's/__CAPTURE__/'"$CAPTURE"'/g' \
+    -e 's/__CAPTURE__/'\"$CAPTURE\"'/g' \
     -e 's/__CLOUDPROVIDER__/'"$CLOUDPROVIDER"'/g' \
     -e 's/__CLUSTER__/'"$CLUSTER"'/g' \
     -e 's/__EMAIL__/'"$EMAIL"'/g' \
+    -e 's/__KUBEMETA_VERSION__/'\"$KUBEMETA_VERSION\"'/g' \
     -e 's/__PLAN__/'"$PLAN"'/g' \
     -e 's/__TOKEN__/'"$TOKEN"'/g' \
     kustomization-template.yml > kustomization.yml
-
-SEDCMD="s/__KUBEMETA_VERSION__/$KUBEMETA_VERSION/"
-OS=$(uname)
-
-# macOS's sed behaves a little differently
-if [[ "$OS" == "Darwin" ]]; then
-    # macOS (BSD sed)
-    sed -i '' "$SED_COMMAND" kubemeta.yml
-else
-    # Linux (GNU sed)
-    sed -i "$SED_COMMAND" kubemeta.yml
-fi
 
 # Set kubeconfig and context variables if provided
 KUBECONF=""
