4b7f39678eb2a00ac8f8e42593471685086b9f5e
Scott Dorr <76019414+sdorr0@users.noreply.github.com>
2023-11-22 12:27:06 -0700

    Initial pass at deployment scripts for kubemeta and easier configuration (#11)
    * Initial pass at deployment scripts for kubemeta and easier configuration
    
    * Add 'capture' (network interface capture) as a config option
    
    * README updates.  Install kubemeta instead of kubeinfo.
    
    * fix default CAPTURE value
    
    * Fix default CAPTURE value for real
    
    * more complete passing of data to kubemeta
    
    * name consistency for API_KEY
    
    * fix multi-line sed
    
    * minor README tweak
    
    * CLOUDPROVIDER must be one of aws, gcp, azure, prem
    
    * renamed kube-cfg -> kentik-kube-deploy
    
    * use tagged version of kubemeta
    
    * macos needs a different sed command
    
    * kubemeta versioning, other tweaks
    
    * re-introduce kubeinfo

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
-# kappa-cfg - kubernetes configs for kappa
+# kentik-kube-deploy - Kube configuration and deployment to Kubernetes
 
-kappa-cfg contains example Kubernetes deployment descriptors for
-kappa. These descriptors utilize kustomize which is available in
-kubectl v1.14+.
+kentik-kube-deploy contains the Kubernetes deployment descriptors necessary to deploy the kube components into a Kubernetes
+cluster. These descriptors utilize kustomize which is available in kubectl v1.14+.
 
-Most user-serviceable settings are configured via the config map
-and secret generators in [kustomization.yml](kustomization.yml).
+Most user-serviceable settings are configured via the configuration section in [deploy-kube.sh](deploy-kube.sh).
+Additional settings can be found in [kustomization-template.yml](kustomization-template.yml).
 
-## usage
+`deploy-kube.sh` is the deployment script.
 
-`kubectl apply -k .`
+To run `deploy-kube.sh`, you'll need to have at least one kubernetes context and your Kentik plan information:
+- Your Kentik Plan ID number
+- Your Kentik registered email address
+- Your Kentik token/API key
+- Your cloud provider name (e.g., aws)
+- Your kubernetes cluster name
+
+## Kube Installation
+### Clone this repo
+```bash
+git clone https://github.com/kentik/kentik-kube-deploy.git
+cd kentik-kube-deploy
+```
+
+### Configure the script
+Edit the `USER CONFIGURATION` section at the very top of `deploy-kube.sh`. This is where you'll set your Kentik plan
+information.
+
+### Run `deploy-kube.sh`
+```bash
+./deploy-kube.sh
+```
+
+## deploy-kube.sh optional flags
+
+`deploy-kube.sh -h` This will show you basic usage of the deployment script.
+
+All the below will deploy kubemeta, kappa agents, and kappa aggregator into a k8s cluster.
+
+- `deploy-kube.sh` Deploy into the k8s cluster as defined by the context in `~/.kube/config`
+- `deploy-kube.sh -f <path-to-kube-config-file>` Deploy into the k8s cluster as defined by the context in the specified config file.
+- `deploy-kube.sh -c <k8s-context>` Deploy into the k8s cluster as defined by a specific context in `~/.kube/config`
+- `deploy-kube.sh -f <path-to-kube-config-file> -c <k8s-context>` Deploy into the k8s cluster as defined by the specified context in the specified config file.
---
+++ deploy-kube.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# ######################
+#
+#   USER CONFIGURATION
+#
+# change the below values to match your kentik plan number, your email/token
+# associated with your kentik account, and the device in question (e.g., a
+# kubernetes cluster)
+#
+# ######################
+#
+# You must set these four as appropriate for your account and device
+#
+PLAN=0123456789
+EMAIL=customer_email@example.com
+TOKEN=123abc_your_customer_kentik_token_cba321
+CLUSTER=your_cluster_name
+CLOUDPROVIDER=aws_or_gcp_or_azure_or_prem
+#
+# Don't change the below values unless you know for sure it's what you want.
+#
+CAPTURE='en*|veth.*|eth*'
+KUBEMETA_VERSION=sha-68b9457
+# ######################
+#
+#   END CONFIGURATION
+#
+# ######################
+
+K8S_CONTEXT=""
+KUBECONFIG_FILE=""
+
+function display_help() {
+    echo "Usage: $0 [-c <k8s context>] [-f <k8s config file>]"
+    echo "  -c  Specify the Kubernetes context. (optional)"
+    echo "  -f  Specify the kubeconfig file to use. (optional)"
+    echo "  -h  Display this help message."
+    exit 1
+}
+
+# some optional commandline args
+while getopts 'c:f:h' flag; do
+    case "${flag}" in
+        c) K8S_CONTEXT="${OPTARG}" ;;
+        f) KUBECONFIG_FILE="${OPTARG}" ;;
+        h) display_help ;;
+        *) display_help ;;
+    esac
+done
+
+# Check for valid CLOUDPROVIDER value
+valid_providers=("aws" "gcp" "azure" "prem")
+is_valid_provider=false
+
+for provider in "${valid_providers[@]}"; do
+    if [[ "$CLOUDPROVIDER" == "$provider" ]]; then
+        is_valid_provider=true
+        break
+    fi
+done
+
+if [[ "$is_valid_provider" == false ]]; then
+    echo
+    echo "Error: Invalid CLOUDPROVIDER value ($CLOUDPROVIDER). Must be one of: aws, gcp, azure, or prem"
+    echo
+    exit 1
+fi
+
+echo "Going to apply the kube yaml configuration with the following values:"
+echo
+echo "Plan:     $PLAN"
+echo "Email:    $EMAIL"
+echo "Token:    $TOKEN"
+echo "Cloud:    $CLOUDPROVIDER"
+echo "Cluster:  $CLUSTER"
+echo
+echo "Capture:  $CAPTURE"
+echo "Kubemeta: $KUBEMETA_VERSION"
+echo
+read -p "Do you want to proceed (y/n)? " confirmation
+
+if [[ ! "${confirmation}" =~ ^[yY]$ ]]; then
+    echo "Aborted."
+    exit 1
+fi
+
+sed \
+    -e 's/__CAPTURE__/'\"$CAPTURE\"'/g' \
+    -e 's/__CLOUDPROVIDER__/'"$CLOUDPROVIDER"'/g' \
+    -e 's/__CLUSTER__/'"$CLUSTER"'/g' \
+    -e 's/__EMAIL__/'"$EMAIL"'/g' \
+    -e 's/__KUBEMETA_VERSION__/'\"$KUBEMETA_VERSION\"'/g' \
+    -e 's/__PLAN__/'"$PLAN"'/g' \
+    -e 's/__TOKEN__/'"$TOKEN"'/g' \
+    kustomization-template.yml > kustomization.yml
+
+# Set kubeconfig and context variables if provided
+KUBECONF=""
+KUBECONTEXT=""
+[[ -n "$KUBECONFIG_FILE" ]] && KUBECONF="KUBECONFIG=\"$KUBECONFIG_FILE\""
+[[ -n "$K8S_CONTEXT" ]] && KUBECONTEXT="--context=\"$K8S_CONTEXT\""
+
+eval $KUBECONF kubectl apply -k . $KUBECONTEXT
