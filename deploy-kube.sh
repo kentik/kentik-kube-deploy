19712a34f7a605926812dbcb9224e99637b3d8ae
Scott Dorr <sdorr@kentik.com>
2024-02-16 16:04:40 -0700

    Region is configurable and modifies grpc endpoint

--- deploy-kube.sh
+++ deploy-kube.sh
@@ -12,17 +12,19 @@
 #
 # You must set these four as appropriate for your account and device
 #
+ACCOUNTREGION=US
 PLAN=0123456789
 EMAIL=customer_email@example.com
 TOKEN=123abc_your_customer_kentik_token_cba321
-CLUSTER=your_cluster_name
 CLOUDPROVIDER=aws_or_gcp_or_azure_or_prem
+CLUSTER=your_cluster_name
 #
 # Don't change the below values unless you know for sure it's what you want.
 #
 CAPTURE='en*|veth.*|eth*'
 KUBEMETA_VERSION=sha-6589ba8
 MAX_PAYLOAD_SIZE_MB=8
+KAPPA_SAMPLE_RATIO=1:4
 # ######################
 #
 #   END CONFIGURATION
@@ -74,15 +76,17 @@ MAX_PAYLOAD_SIZE_BYTES=$((MAX_PAYLOAD_SIZE_MB * 1024 * 1024))
 
 echo "Going to apply the kube yaml configuration with the following values:"
 echo
-echo "Plan:       $PLAN"
-echo "Email:      $EMAIL"
-echo "Token:      $TOKEN"
-echo "Cloud:      $CLOUDPROVIDER"
-echo "Cluster:    $CLUSTER"
+echo "Account Region: $ACCOUNTREGION"
+echo "Plan:           $PLAN"
+echo "Email:          $EMAIL"
+echo "Token:          $TOKEN"
+echo "Cloud:          $CLOUDPROVIDER"
+echo "Cluster:        $CLUSTER"
 echo
-echo "Capture:    $CAPTURE"
-echo "Kubemeta:   $KUBEMETA_VERSION"
-echo "MaxPayload: ${MAX_PAYLOAD_SIZE_MB}MB"
+echo "Capture:        $CAPTURE"
+echo "Kappa Sample:   $KAPPA_SAMPLE_RATIO"
+echo "Kubemeta:       $KUBEMETA_VERSION"
+echo "MaxPayload:     ${MAX_PAYLOAD_SIZE_MB}MB"
 echo
 read -p "Do you want to proceed (y/n)? " confirmation
 
@@ -91,11 +95,27 @@ if [[ ! "${confirmation}" =~ ^[yY]$ ]]; then
     exit 1
 fi
 
+case $ACCOUNTREGION in
+    US)
+        GRPCENDPOINT="grpc.api.kentik.com"
+        ;;
+    EU)
+        GRPCENDPOINT="grpc.api.kentik.eu"
+        ;;
+    *)
+        echo "Unknown account region: $ACCOUNTREGION"
+        exit 1
+        ;;
+esac
+
 sed \
+    -e 's/__ACCOUNTREGION__/'"$ACCOUNTREGION"'/g' \
     -e 's/__CAPTURE__/'\"$CAPTURE\"'/g' \
     -e 's/__CLOUDPROVIDER__/'"$CLOUDPROVIDER"'/g' \
     -e 's/__CLUSTER__/'"$CLUSTER"'/g' \
     -e 's/__EMAIL__/'"$EMAIL"'/g' \
+    -e 's/__GRPCENDPOINT__/'"$GRPCENDPOINT"'/g' \
+    -e 's/__KAPPA_SAMPLE_RATIO__/'"$KAPPA_SAMPLE_RATIO"'/g' \
     -e 's/__KUBEMETA_VERSION__/'\"$KUBEMETA_VERSION\"'/g' \
     -e 's/__MAXGRPCPAYLOAD__/'"$MAX_PAYLOAD_SIZE_BYTES"'/g' \
     -e 's/__PLAN__/'"$PLAN"'/g' \
